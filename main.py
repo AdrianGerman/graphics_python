06f71005ae77c6be84003543f4a4fb8baf9c42c4
AdrianGerman <adrian.german1019@gmail.com>
2024-02-13 08:22:53 -0600

    init proyect

---
+++ main.py
@@ -0,0 +1,20 @@
+from app import utils
+
+keys, values = utils.get_population()
+print(keys, values)
+
+data = [
+    {
+        'Country': 'Mexico',
+        'Population': 500
+    },
+    {
+        'Country': 'Paraguay',
+        'Population': 300
+    }
+]
+
+country = input('Escribe el país => ')
+
+result = utils.population_by_country(data, country)
+print(result)
