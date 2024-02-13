68afb3327f0bd1c46ef28a9ddb081e81f2ecac18
AdrianGerman <adrian.german1019@gmail.com>
2024-02-13 10:49:14 -0600

    creating a graph

--- charts.py
+++ charts.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-x = np.linspace(0, 2 * np.pi, 200)
-y = np.sin(x)
+def generate_bar_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    plt.show()
 
-fig, ax = plt.subplots()
-ax.plot(x, y)
-plt.show()
+def generate_pie_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    ax.axis('equal')
+    plt.show()
+    
+if __name__ == '__main__':
+    labels = ['a', 'b', 'c']
+    values = [10, 40, 800]
+    generate_pie_chart(labels, values)
